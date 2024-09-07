50326bf8a3556e4f2b9f129677e7fe4d0e9c905a
Ms7Biel <gabriel.msantos7@hotmail.com>
2024-09-07 10:10:43 -0300

    aula 04 e inicio da aula 5 da imersão dev-alura-gemini

--- jogadores.js
+++ jogadores.js
@@ -1,50 +1,326 @@
-let dados = [
+let jogador = [
   {
-    titulo: "Allen Iverson",
+    nome: "Allen Iverson",
     descricao:
       "Allen Iverson foi um dos maiores jogadores de basquete da sua geração. Conhecido por sua habilidade de driblar, sua baixa estatura e sua personalidade marcante, Iverson se tornou um ícone da NBA.",
     link: "https://pt.wikipedia.org/wiki/Allen_Iverson",
   },
   {
-    titulo: "LeBron James",
+    nome: "LeBron James",
     descricao:
       "LeBron James é um dos maiores atletas da história, conhecido por sua versatilidade e longevidade na NBA. Com múltiplos títulos e recordes, ele é frequentemente comparado a Michael Jordan na discussão sobre o maior jogador de todos os tempos.",
     link: "https://pt.wikipedia.org/wiki/LeBron_James",
   },
   {
-    titulo: "Michael Jordan",
+    nome: "Michael Jordan",
     descricao:
       "Considerado por muitos o maior jogador de basquete de todos os tempos, Michael Jordan conquistou seis títulos da NBA com o Chicago Bulls. Sua habilidade de voar pelos ares e sua mentalidade vencedora o tornaram uma lenda do esporte.",
     link: "https://pt.wikipedia.org/wiki/Michael_Jordan",
   },
   {
-    titulo: "Kobe Bryant",
+    nome: "Kobe Bryant",
     descricao:
       "Kobe Bryant, conhecido como 'Black Mamba', foi um dos jogadores mais talentosos e competitivos da história da NBA. Ele conquistou cinco títulos com o Los Angeles Lakers e deixou um legado de trabalho duro e dedicação.",
     link: "https://pt.wikipedia.org/wiki/Kobe_Bryant",
   },
   {
-    titulo: "Magic Johnson",
+    nome: "Magic Johnson",
     descricao:
       "Magic Johnson revolucionou o jogo com sua habilidade de passar e sua visão de quadra. Como armador do Los Angeles Lakers, ele ganhou cinco títulos da NBA e se tornou um dos maiores jogadores de todos os tempos.",
     link: "https://pt.wikipedia.org/wiki/Magic_Johnson",
   },
   {
-    titulo: "Larry Bird",
+    nome: "Larry Bird",
     descricao:
       "Larry Bird foi um dos maiores competidores e atiradores da NBA. Ele liderou o Boston Celtics a três campeonatos da NBA e teve uma rivalidade lendária com Magic Johnson.",
     link: "https://pt.wikipedia.org/wiki/Larry_Bird",
   },
   {
-    titulo: "Shaquille O'Neal",
+    nome: "Shaquille O'Neal",
     descricao:
       "Shaquille O'Neal, também conhecido como Shaq, dominou as quadras da NBA com sua força e tamanho. Ele conquistou quatro títulos da NBA e se tornou um dos maiores pivôs de todos os tempos.",
     link: "https://pt.wikipedia.org/wiki/Shaquille_O'Neal",
   },
   {
-    titulo: "Tim Duncan",
+    nome: "Tim Duncan",
     descricao:
       "Tim Duncan, apelidado de 'The Big Fundamental', é amplamente considerado um dos melhores jogadores de basquete da história. Ele liderou o San Antonio Spurs a cinco títulos da NBA e foi conhecido por sua eficiência e liderança tranquila.",
     link: "https://pt.wikipedia.org/wiki/Tim_Duncan",
+  },
+  {
+    nome: "Dominique Wilkins",
+    descricao:
+      "Dominique Wilkins, conhecido como 'The Human Highlight Film', foi um dos jogadores mais emocionantes da história da NBA, famoso por suas enterradas acrobáticas.",
+    link: "https://pt.wikipedia.org/wiki/Dominique_Wilkins",
+  },
+  {
+    nome: "George Gervin",
+    descricao:
+      "George Gervin, apelidado de 'The Iceman', foi um dos maiores pontuadores da NBA e conhecido por seu arremesso suave, o 'finger roll'.",
+    link: "https://pt.wikipedia.org/wiki/George_Gervin",
+  },
+  {
+    nome: "Walt Frazier",
+    descricao:
+      "Walt Frazier foi um dos maiores armadores da história do New York Knicks, liderando a equipe a dois títulos da NBA e sendo conhecido por seu estilo e defesa.",
+    link: "https://pt.wikipedia.org/wiki/Walt_Frazier",
+  },
+  {
+    nome: "Bob Cousy",
+    descricao:
+      "Bob Cousy foi um dos primeiros grandes armadores da NBA, vencendo seis títulos da NBA com o Boston Celtics e revolucionando o jogo com suas assistências criativas.",
+    link: "https://pt.wikipedia.org/wiki/Bob_Cousy",
+  },
+  {
+    nome: "Kevin McHale",
+    descricao:
+      "Kevin McHale foi um dos maiores ala-pivôs da história da NBA, vencendo três títulos com o Boston Celtics e sendo conhecido por seu arsenal de movimentos no garrafão.",
+    link: "https://pt.wikipedia.org/wiki/Kevin_McHale",
+  },
+  {
+    nome: "Pete Maravich",
+    descricao:
+      "Pete Maravich, também conhecido como 'Pistol Pete', foi um dos jogadores mais criativos e habilidosos da NBA, famoso por seus passes e arremessos ousados.",
+    link: "https://pt.wikipedia.org/wiki/Pete_Maravich",
+  },
+  {
+    nome: "Dikembe Mutombo",
+    descricao:
+      "Dikembe Mutombo foi um dos maiores defensores e bloqueadores da história da NBA, famoso por seu gesto de 'não' com o dedo após bloquear arremessos.",
+    link: "https://pt.wikipedia.org/wiki/Dikembe_Mutombo",
+  },
+  {
+    nome: "Rick Barry",
+    descricao:
+      "Rick Barry foi um dos maiores alas da história da NBA, conhecido por seu arremesso de lance livre debaixo para cima e por liderar o Golden State Warriors a um título da NBA.",
+    link: "https://pt.wikipedia.org/wiki/Rick_Barry",
+  },
+  {
+    nome: "Bob Pettit",
+    descricao:
+      "Bob Pettit foi um dos primeiros grandes jogadores da NBA, sendo o primeiro MVP da liga e liderando os St. Louis Hawks a um título da NBA.",
+    link: "https://pt.wikipedia.org/wiki/Bob_Pettit",
+  },
+  {
+    nome: "Elvin Hayes",
+    descricao:
+      "Elvin Hayes, conhecido como 'The Big E', foi um dos maiores alas-pivôs da história da NBA, vencendo um título com o Washington Bullets.",
+    link: "https://pt.wikipedia.org/wiki/Elvin_Hayes",
+  },
+  {
+    nome: "Moses Malone",
+    descricao:
+      "Moses Malone foi um dos maiores pivôs da NBA, três vezes MVP e campeão com o Philadelphia 76ers em 1983, famoso por sua habilidade de pegar rebotes.",
+    link: "https://pt.wikipedia.org/wiki/Moses_Malone",
+  },
+  {
+    nome: "Wes Unseld",
+    descricao:
+      "Wes Unseld foi um dos maiores defensores e reboteiros da NBA, liderando o Washington Bullets a um título da NBA e sendo um MVP das finais.",
+    link: "https://pt.wikipedia.org/wiki/Wes_Unseld",
+  },
+  {
+    nome: "Nate Thurmond",
+    descricao:
+      "Nate Thurmond foi um dos maiores pivôs defensivos da NBA, sendo um dos primeiros a registrar um quádruplo-duplo e um reboteiro feroz.",
+    link: "https://pt.wikipedia.org/wiki/Nate_Thurmond",
+  },
+  {
+    nome: "James Worthy",
+    descricao:
+      "James Worthy foi uma peça fundamental nos 'Showtime' Lakers, vencendo três títulos da NBA e sendo MVP das finais em 1988.",
+    link: "https://pt.wikipedia.org/wiki/James_Worthy",
+  },
+  {
+    nome: "Clyde Lovellette",
+    descricao:
+      "Clyde Lovellette foi um dos primeiros jogadores a vencer tanto no nível universitário quanto na NBA, sendo um dos grandes pivôs de sua era.",
+    link: "https://pt.wikipedia.org/wiki/Clyde_Lovellette",
+  },
+  {
+    nome: "Hal Greer",
+    descricao:
+      "Hal Greer foi uma das estrelas do Philadelphia 76ers, ajudando a equipe a vencer o título da NBA em 1967 e sendo um dos grandes pontuadores de sua geração.",
+    link: "https://pt.wikipedia.org/wiki/Hal_Greer",
+  },
+  {
+    nome: "Dave Cowens",
+    descricao:
+      "Dave Cowens foi um pivô versátil que liderou o Boston Celtics a dois títulos da NBA e foi duas vezes MVP da liga.",
+    link: "https://pt.wikipedia.org/wiki/Dave_Cowens",
+  },
+  {
+    nome: "Bill Walton",
+    descricao:
+      "Bill Walton foi um dos maiores pivôs da história do basquete, vencedor de dois títulos da NBA e conhecido por seu QI de basquete e visão de jogo.",
+    link: "https://pt.wikipedia.org/wiki/Bill_Walton",
+  },
+  {
+    nome: "Adrian Dantley",
+    descricao:
+      "Adrian Dantley foi um dos maiores pontuadores da história da NBA, conhecido por sua eficiência nos arremessos e pela habilidade de marcar pontos no garrafão.",
+    link: "https://pt.wikipedia.org/wiki/Adrian_Dantley",
+  },
+  {
+    nome: "Artis Gilmore",
+    descricao:
+      "Artis Gilmore foi um dos pivôs mais dominantes da ABA e da NBA, conhecido por sua força e eficiência nos arremessos, sendo um dos líderes em rebotes e bloqueios.",
+    link: "https://pt.wikipedia.org/wiki/Artis_Gilmore",
+  },
+  {
+    nome: "Gail Goodrich",
+    descricao:
+      "Gail Goodrich foi um dos maiores armadores da NBA, ajudando o Los Angeles Lakers a vencer um título e sendo um dos principais artilheiros de sua era.",
+    link: "https://pt.wikipedia.org/wiki/Gail_Goodrich",
+  },
+  {
+    nome: "Dennis Johnson",
+    descricao:
+      "Dennis Johnson foi um dos maiores defensores de perímetro da NBA, vencendo três títulos e sendo MVP das finais com o Seattle SuperSonics.",
+    link: "https://pt.wikipedia.org/wiki/Dennis_Johnson",
+  },
+  {
+    nome: "Alex English",
+    descricao:
+      "Alex English foi um dos melhores artilheiros da NBA nos anos 80, liderando o Denver Nuggets a várias aparições nos playoffs com seu jogo de perímetro suave.",
+    link: "https://pt.wikipedia.org/wiki/Alex_English",
+  },
+  {
+    nome: "Bernard King",
+    descricao:
+      "Bernard King foi um dos alas mais prolíficos da história da NBA, conhecido por sua habilidade de marcar pontos em sequência e ser um dos jogadores mais difíceis de marcar.",
+    link: "https://pt.wikipedia.org/wiki/Bernard_King",
+  },
+  {
+    nome: "Maurice Cheeks",
+    descricao:
+      "Maurice Cheeks foi um dos melhores armadores defensivos da NBA, vencendo um título com o Philadelphia 76ers e sendo um dos grandes líderes em assistências.",
+    link: "https://pt.wikipedia.org/wiki/Maurice_Cheeks",
+  },
+  {
+    nome: "Paul Westphal",
+    descricao:
+      "Paul Westphal foi um dos maiores armadores dos anos 70 e 80, levando o Phoenix Suns às finais da NBA e sendo um dos principais artilheiros de sua era.",
+    link: "https://pt.wikipedia.org/wiki/Paul_Westphal",
+  },
+  {
+    nome: "Spencer Haywood",
+    descricao:
+      "Spencer Haywood foi um dos primeiros a desafiar as regras de elegibilidade da NBA, tendo uma carreira produtiva e vencendo um título com o Los Angeles Lakers.",
+    link: "https://pt.wikipedia.org/wiki/Spencer_Haywood",
+  },
+  {
+    nome: "Jack Sikma",
+    descricao:
+      "Jack Sikma foi um dos maiores pivôs arremessadores da NBA, conhecido por sua habilidade de jogar longe do garrafão e por vencer um título com o Seattle SuperSonics.",
+    link: "https://pt.wikipedia.org/wiki/Jack_Sikma",
+  },
+  {
+    nome: "Sidney Moncrief",
+    descricao:
+      "Sidney Moncrief foi um dos melhores defensores de perímetro da NBA, vencendo dois prêmios de Jogador Defensivo do Ano e sendo um dos líderes do Milwaukee Bucks.",
+    link: "https://pt.wikipedia.org/wiki/Sidney_Moncrief",
+  },
+  {
+    nome: "Chris Mullin",
+    descricao:
+      "Chris Mullin foi um dos maiores alas arremessadores da NBA, membro do Dream Team e um dos principais jogadores do Golden State Warriors.",
+    link: "https://pt.wikipedia.org/wiki/Chris_Mullin",
+  },
+  {
+    nome: "Reggie Miller",
+    descricao:
+      "Reggie Miller foi um dos maiores arremessadores de três pontos da história da NBA, conhecido por seu desempenho em momentos decisivos e sua rivalidade com o New York Knicks.",
+    link: "https://pt.wikipedia.org/wiki/Reggie_Miller",
+  },
+  {
+    nome: "Ray Allen",
+    descricao:
+      "Ray Allen foi um dos maiores arremessadores da NBA, estabelecendo o recorde de mais cestas de três pontos feitas e vencendo dois títulos da NBA.",
+    link: "https://pt.wikipedia.org/wiki/Ray_Allen",
+  },
+  {
+    nome: "Yao Ming",
+    descricao:
+      "Yao Ming foi um dos jogadores mais dominantes e influentes da NBA, sendo o embaixador do basquete na China e um dos melhores pivôs de sua geração.",
+    link: "https://pt.wikipedia.org/wiki/Yao_Ming",
+  },
+  {
+    nome: "David Thompson",
+    descricao:
+      "David Thompson foi uma das maiores estrelas da ABA e da NBA, conhecido por sua capacidade de saltar e por inspirar o estilo de jogo de Michael Jordan.",
+    link: "https://pt.wikipedia.org/wiki/David_Thompson",
+  },
+  {
+    nome: "Grant Hill",
+    descricao:
+      "Grant Hill foi um dos jogadores mais versáteis da NBA, superando diversas lesões para ter uma carreira duradoura e sendo um modelo de esportividade.",
+    link: "https://pt.wikipedia.org/wiki/Grant_Hill",
+  },
+  {
+    nome: "Dražen Petrović",
+    descricao:
+      "Dražen Petrović foi um dos maiores jogadores europeus a jogar na NBA, sendo um dos melhores arremessadores de sua era antes de sua carreira ser interrompida por um trágico acidente.",
+    link: "https://pt.wikipedia.org/wiki/Dražen_Petrović",
+  },
+  {
+    nome: "Arvydas Sabonis",
+    descricao:
+      "Arvydas Sabonis foi um dos maiores pivôs da história do basquete europeu, e quando chegou à NBA, mostrou sua incrível visão de jogo e habilidades de passe.",
+    link: "https://pt.wikipedia.org/wiki/Arvydas_Sabonis",
+  },
+  {
+    nome: "Vlade Divac",
+    descricao:
+      "Vlade Divac foi um dos primeiros europeus a ter sucesso na NBA, conhecido por seu passe brilhante e inteligência de jogo como pivô.",
+    link: "https://pt.wikipedia.org/wiki/Vlade_Divac",
+  },
+  {
+    nome: "Ralph Sampson",
+    descricao:
+      "Ralph Sampson foi um dos pivôs mais talentosos da NBA, conhecido por sua altura e agilidade, vencendo o prêmio de Novato do Ano e sendo MVP do All-Star Game.",
+    link: "https://pt.wikipedia.org/wiki/Ralph_Sampson",
+  },
+  {
+    nome: "Oscar Robertson",
+    descricao:
+      "Oscar Robertson, conhecido como 'The Big O', foi um dos jogadores mais completos da história da NBA, sendo o primeiro a promediar um triplo-duplo por uma temporada.",
+    link: "https://pt.wikipedia.org/wiki/Oscar_Robertson",
+  },
+  {
+    nome: "Chet Walker",
+    descricao:
+      "Chet Walker foi um dos grandes alas da NBA nos anos 60 e 70, conhecido por sua habilidade de marcar pontos e sua consistência em momentos decisivos.",
+    link: "https://pt.wikipedia.org/wiki/Chet_Walker",
+  },
+  {
+    nome: "Jo Jo White",
+    descricao:
+      "Jo Jo White foi um armador habilidoso e vencedor de dois títulos da NBA com o Boston Celtics, conhecido por sua habilidade de arremesso e liderança.",
+    link: "https://pt.wikipedia.org/wiki/Jo_Jo_White",
+  },
+  {
+    nome: "Kareem Abdul-Jabbar",
+    descricao:
+      "Kareem Abdul-Jabbar é o maior pontuador da história da NBA e dono do icônico arremesso 'skyhook', sendo um dos jogadores mais dominantes de todos os tempos.",
+    link: "https://pt.wikipedia.org/wiki/Kareem_Abdul-Jabbar",
+  },
+  {
+    nome: "Patrick Ewing",
+    descricao:
+      "Patrick Ewing foi um dos maiores pivôs da NBA, liderando o New York Knicks durante os anos 90 e sendo uma presença dominante no garrafão.",
+    link: "https://pt.wikipedia.org/wiki/Patrick_Ewing",
+  },
+  {
+    nome: "John Stockton",
+    descricao:
+      "John Stockton é o maior assistente da história da NBA, famoso por sua durabilidade e liderança no Utah Jazz ao lado de Karl Malone.",
+    link: "https://pt.wikipedia.org/wiki/John_Stockton",
+  },
+  {
+    nome: "Karl Malone",
+    descricao:
+      "Karl Malone, conhecido como 'The Mailman', é o segundo maior pontuador da história da NBA e formou uma dupla histórica com John Stockton no Utah Jazz.",
+    link: "https://pt.wikipedia.org/wiki/Karl_Malone",
   },
 ];
--- script.js
+++ script.js
@@ -1,1 +1,31 @@
-console.log(`O título é: ${dados[0].titulo} e o link é: ${dados[0].link}`);
+// Função para pesquisar um jogador e exibir os resultados na página
+function pesquisar() {
+  // Seleciona a seção onde os resultados serão exibidos
+  let section = document.getElementById("resultados-pesquisa");
+
+  let campo_pesquisa = document.getElementById("campo-pesquisa");
+
+  // Inicializa uma string vazia para armazenar os resultados
+  let resultados = "";
+
+  // Itera sobre cada jogador no array de jogadores
+  for (let dados of jogador) {
+    // Cria uma nova div para cada jogador encontrado
+    resultados += `
+        <div class="item-resultado">
+          <h2>
+            <a href="#" target="_blank">${dados.nome}</a>
+          </h2>
+          <p class="discricao-meta">
+            ${dados.descricao}
+          </p>
+          <a href="${dados.link}" target="_blank" rel="external">
+            Mais Informações sobre - ${dados.nome}
+          </a>
+        </div>
+      `;
+  }
+
+  // Atribui a string com os resultados ao conteúdo da seção
+  section.innerHTML = resultados;
+}
